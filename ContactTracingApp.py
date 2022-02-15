2754db7ca399c63f0cf9645cb87a64679a0346b3
cirsjulyancolesco <crisjulyancolesco@iskolarngbayan.pup.edu.ph>
2022-02-15 15:09:08 +0800

    Opening the Webcam

---
+++ ContactTracingApp.py
@@ -0,0 +1,27 @@
+#Contact Tracing App
+#	- Create a python program that will read QRCode using your webcam
+#	- You may use any online QRCode generator to create QRCode
+#	- All personal data are in QRCode 
+#	- You may decide which personal data to include
+#	- All data read from QRCode should be stored in a text file including the date and time it was read
+#Note: 
+#	- Search how to generate QRCode
+#	- Search how to read QRCode using webcam
+#	- Search how to create and write to text file
+#	- Your source code should be in github before Feb 19
+#	- Create a demo of your program (1-2 min) and send it directly to my messenger.
+
+import cv2
+
+Cap = cv2.VideoCapture(0)
+while True:
+    _, Cam = Cap.read()
+
+    cv2.imshow("Webcam", Cam)
+
+    Key = cv2.waitKey(1)
+    if Key == 27:
+        break
+
+Cap.release()
+cv2.destroyAllWindows
