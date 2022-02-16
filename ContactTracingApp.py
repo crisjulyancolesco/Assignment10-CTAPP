6427893d8c1f1b74a12940a4423db353bd90ba6c
cirsjulyancolesco <crisjulyancolesco@iskolarngbayan.pup.edu.ph>
2022-02-16 14:12:27 +0800

    Final Working Contact Tracing App Program

--- ContactTracingApp.py
+++ ContactTracingApp.py
@@ -1,16 +1,3 @@
-#Contact Tracing App
-#	- Create a python program that will read QRCode using your webcam
-#	- You may use any online QRCode generator to create QRCode
-#	- All personal data are in QRCode 
-#	- You may decide which personal data to include
-#	- All data read from QRCode should be stored in a text file including the date and time it was read
-#Note: 
-#	- Search how to generate QRCode
-#	- Search how to read QRCode using webcam
-#	- Search how to create and write to text file
-#	- Your source code should be in github before Feb 19
-#	- Create a demo of your program (1-2 min) and send it directly to my messenger.
-
 import cv2
 import pyzbar.pyzbar as pyzbar
 import time
@@ -50,4 +37,4 @@ File = open("test.txt", "a")
 File.write(str(DecodedInfo) + "\n")
 File.write("Date Visited: " + DateToday + "\n")
 File.write("Time Visited: " + TimeNow + "\n")
-File.write("" + "\n")
+File.write("" + "\n") # For spaces after each QR code data
